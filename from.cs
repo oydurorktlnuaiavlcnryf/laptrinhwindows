247ae23e88562adfd9eeda0d305e1eaf495b52d0
Huyen Trang <oydurorktlnuaiavlcnryf@ns.do.edu.vn>
2025-10-25 23:38:50 +0700

    Add Form1 class with greeting message on load

---
+++ from.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Windows.Forms;
+
+namespace HelloWindows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Xin chào Windows!");
+        }
+    }
+}
